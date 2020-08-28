a9a99b74d370b92115acb421da18ae546b1d7651
bozbi <bosbizz@gmail.com>
2020-08-28 15:30:24 +0200

    typo

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require("express")
-app = express();
+app = express()
 
-app.set("view engine", "ejs");
+app.set("view engine", "ejs")
 
 app.get("/",(req,res)=>{
+    res.send("welcome to bosbizz.fr")
+})
+
+app.get("/resume/policy",(req,res)=>{
     res.render("resume-app-privacy-policy");
-});
+})
 
 app.listen(8080,()=>{
-    console.log("Server is running on port 8080");
-});
+    console.log("Server is running on port 8080")
+})
