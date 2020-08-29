5faecc3aa2563f8613cf89801e37e960cf167bf6
bozbi <bosbizz@gmail.com>
2020-08-29 20:09:50 +0200

    typo

--- app.js
+++ app.js
@@ -11,6 +11,6 @@ app.get("/resume/policy",(req,res)=>{
     res.render("resume-app-privacy-policy");
 })
 
-app.listen(8080,"localhost",()=>{
+app.listen(8080,()=>{
     console.log("Server is running on port 8080")
 })
