4c714e5f0b399cf7c947e2863983ff751c892b16
bozbi <bosbizz@gmail.com>
2020-09-15 13:39:17 +0200

    added Awesome Flow apk

--- app.js
+++ app.js
@@ -18,6 +18,10 @@ app.get("/resume/privacy",(req,res)=>{
     res.render("resume-app-privacy-policy");
 })
 
+app.get("/download/awesome_flow",(req,res)=>{
+    res.download(__dirname+'/public/apks/awesome_flow.apk')
+})
+
 app.listen(port,'localhost',()=>{
     console.log("Server is running on port 8080")
 })
